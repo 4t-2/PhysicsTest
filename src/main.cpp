3efce597cce15efb77d5ca0eb6110b424affa388
Synteng <denisjacuk07@gmail.com>
2023-02-28 22:43:44 +0000

    Added PhySim

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "../lib/AGL/agl.hpp"
+#include <vector>
 
 class Circle : public agl::Drawable
 {
@@ -7,12 +8,19 @@ class Circle : public agl::Drawable
 		agl::Vec<float, 2> velocity;
 		agl::Vec<float, 2> acceleration;
 
-		float radius = 100;
+		float radius = 1;
 
 		agl::Circle &shape;
 
 	public:
-		Circle(agl::Circle &shape) : shape(shape){}
+		Circle(agl::Circle &shape) : shape(shape)
+		{
+		}
+
+		void setRadius(float radius)
+		{
+			this->radius = radius;
+		}
 
 		void setPosition(agl::Vec<float, 2> position)
 		{
@@ -31,6 +39,7 @@ class Circle : public agl::Drawable
 
 		void update()
 		{
+			velocity = velocity + acceleration;
 			position = position + velocity;
 		}
 
@@ -44,12 +53,55 @@ class Circle : public agl::Drawable
 		}
 };
 
+class PhySim : public agl::Drawable
+{
+	private:
+		std::vector<Circle> circle;
+		agl::Circle		   &shape;
+		float gravity;
+
+	public:
+		PhySim(agl::Circle &shape) : shape(shape)
+		{
+		}
+
+		void setGravity(float gravity)
+		{
+			this->gravity = gravity;
+		}
+
+		void addCircle(agl::Vec<float, 2> position, agl::Vec<float, 2> velocity, float radius)
+		{
+			circle.emplace_back(Circle(shape));
+			circle[circle.size()-1].setPosition(position);
+			circle[circle.size()-1].setVelocity(velocity);
+			circle[circle.size()-1].setRadius(radius);
+		}
+
+		void update()
+		{
+			for (Circle &circle : circle)
+			{
+				circle.setAcceleration({0, gravity});
+				circle.update();
+			}
+		}
+
+		void drawFunction(agl::RenderWindow &window) override
+		{
+			for (Circle &circle : circle)
+			{
+				window.draw(circle);
+			}
+		}
+};
+
 int main()
 {
 	agl::RenderWindow window;
 	window.setup({1920, 1080}, "PhysicsTest");
 	window.setFPS(60);
-	window.setClearColor(agl::Color::Gray);
+	window.setClearColor(agl::Color::Black);
 
 	agl::ShaderBuilder vert;
 	vert.setDefaultVert();
@@ -73,12 +125,18 @@ int main()
 	agl::Event event;
 	event.setWindow(window);
 
+	agl::Texture blank;
+	blank.setBlank();
+
 	agl::Circle shape(32);
+	shape.setTexture(&blank);
+	shape.setColor(agl::Color::White);
 
-	Circle circle(shape);
-	circle.setVelocity({1, 0});
+	PhySim phySim(shape);
+	phySim.setGravity(0.1);
 
-	circle.setPosition({1000, 1000});
+	phySim.addCircle({500, 500}, {3, -9}, 10);
+	phySim.addCircle({400, 500}, {3, -3}, 10);
 
 	while (!event.windowClose())
 	{
@@ -86,11 +144,11 @@ int main()
 
 		window.clear();
 
-		window.draw(circle);
+		window.draw(phySim);
 
 		window.display();
 
-		circle.update();
+		phySim.update();
 	}
 
 	window.close();
